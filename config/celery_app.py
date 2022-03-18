8081b6fc260423ddb4cf2e560029b549431e3949
lucas <lluc23@hotmail.com>
2022-03-18 17:45:25 +0100

    .

--- config/celery_app.py
+++ config/celery_app.py
@@ -3,7 +3,10 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+try:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+except:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 app = Celery("apps")
 
